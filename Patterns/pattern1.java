3ee536b01de664d1ad1b2b182fd3100be722538d
Sanuth Bibin.B <165442353+Sanuthb@users.noreply.github.com>
2025-01-01 11:14:52 +0530

    first commit

---
+++ Patterns/pattern1.java
@@ -0,0 +1,14 @@
+// Solid rectangle
+// *****
+// *****
+// *****
+class pattern1{
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Patterns/pattern7.java
@@ -0,0 +1,39 @@
+public class pattern7 {
+    public static void main(String[] args) {
+        int n = 5; // Number of rows for half the butterfly
+
+        // Upper half of the butterfly
+        for (int i = 1; i <= n; i++) {
+            // Left stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // Middle spaces
+            for (int k = 1; k <= 2 * (n - i); k++) {
+                System.out.print(" ");
+            }
+            // Right stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Lower half of the butterfly
+        for (int i = n; i >= 1; i--) {
+            // Left stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // Middle spaces
+            for (int k = 1; k <= 2 * (n - i); k++) {
+                System.out.print(" ");
+            }
+            // Right stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
